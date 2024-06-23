001c161aff5d36fbe502dd157d06bae648a89885
Malik Bennett <79369570+Rebrodie@users.noreply.github.com>
2024-06-23 04:00:35 -0500

    Step 2 completed

--- getPlan.js
+++ getPlan.js
@@ -1,47 +1,39 @@
 export default function getPlans(onPlanChange, onPriceChange) {
   const plans = [...document.querySelectorAll("[data-plan]")];
 
-  const arcade = plans[0];
-  const arcadePrice = arcade.querySelector("[data-price]");
-
-  const advanced = plans[1];
-  const advancedPrice = advanced.querySelector("[data-price]");
-
-  const pro = plans[2];
-  const proPrice = pro.querySelector("[data-price]");
-
   const toggleSection = document.querySelector("[data-billingToggler]");
   const check = document.getElementById("billing");
-
-  // Set initial billing state
-  check.checked = true;
 
   const monthly = toggleSection.querySelector("[data-billing='monthly']");
   const yearly = toggleSection.querySelector("[data-billing='yearly']");
 
   let currentPlanElement = null;
 
+  function Plans(planIndex, price, isMonthly) {
+    const priceElement = plans[planIndex].querySelector("[data-price]");
+    priceElement.dataset.price = price;
+    if (isMonthly) {
+      priceElement.textContent = `$${price}/mo`;
+    } else {
+      priceElement.textContent = `$${price}/yr`;
+    }
+    monthly.classList.toggle("selectedPlan");
+    yearly.classList.toggle("selectedPlan");
+  }
+
   // Function to update prices based on the current billing period
   const updatePrices = () => {
-    if (check.checked) {
-      arcadePrice.dataset.price = 9;
-      arcadePrice.textContent = `$${arcadePrice.dataset.price}/mo`;
-      advancedPrice.dataset.price = 15;
-      advancedPrice.textContent = `$${advancedPrice.dataset.price}/mo`;
-      proPrice.dataset.price = 12;
-      proPrice.textContent = `$${proPrice.dataset.price}/mo`;
-      monthly.classList.add("selectedPlan");
-      yearly.classList.remove("selectedPlan");
+    if (!check.checked) {
+      Plans(0, 9, true);
+      Plans(1, 12, true);
+      Plans(2, 15, true);
+
       console.log("Billing Monthly");
     } else {
-      arcadePrice.dataset.price = 90;
-      arcadePrice.textContent = `$${arcadePrice.dataset.price}/yr`;
-      advancedPrice.dataset.price = 120;
-      advancedPrice.textContent = `$${advancedPrice.dataset.price}/yr`;
-      proPrice.dataset.price = 150;
-      proPrice.textContent = `$${proPrice.dataset.price}/yr`;
-      monthly.classList.remove("selectedPlan");
-      yearly.classList.add("selectedPlan");
+      Plans(0, 90, false);
+      Plans(1, 120, false);
+      Plans(2, 150, false);
+
       console.log("Billing Yearly");
     }
 
